e1daa90c857ee52188fbd0cab5d6de951ee8d253
fabio bueno <fabiobueno@outlook.com>
2024-10-17 20:52:40 -0300

    Implementando fetch projetos na página My Projects

--- src/pages/MyProjects/index.jsx
+++ src/pages/MyProjects/index.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
-import { Container, Box, Title } from '@mantine/core';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { userProjectsInfos } from '../../store/actions/userProjects';
+import { Container, Group, Box, Title, Button, Text, Image } from '@mantine/core';
 import Header from '../../components/header';
 import FooterMenuMobile from '../../components/footerMenuMobile';
 
 function MyProjects () {
+
+  let dispatch = useDispatch();
+  const loggedUser = JSON.parse(localStorage.getItem('user'));
+  const projects = useSelector(state => state.userProjects);
+
+  console.log(15, projects);
+
+  useEffect(() => {
+    dispatch(userProjectsInfos.getUserProjects(loggedUser.id,'all'));
+  }, [loggedUser.id, dispatch]);
+
+  const cdnBaseURL = 'https://ik.imagekit.io/mublin/';
+  const cdnProjectPath = cdnBaseURL+'projects/tr:h-250,w-410,fo-top,c-maintain_ratio/';
 
   return (
     <>
@@ -12,6 +27,22 @@ function MyProjects () {
         <Box mb={24}>
           <Title order={3}>Meus Projetos</Title>
         </Box>
+        {projects?.list.map((project) => (
+          <Group mb={10}>
+            <Image
+              src={cdnProjectPath+project?.picture}
+              height={160}
+              alt={project?.name}
+              radius={10}
+            />
+            <div>
+              <Title size="h4">{project.name}</Title>
+              <Text size="h4">{project.ptname}</Text>
+            </div>
+            {/* <Title size="h4">{project.name}</Title>
+            <Button variant="default">{project.ptname}</Button> */}
+          </Group>
+        ))}
       </Container>
       <FooterMenuMobile />
     </>
