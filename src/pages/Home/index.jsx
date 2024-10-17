b2851ed289fd58cdfacb73b33c9740cc4b115741
fabio bueno <fabiobueno@outlook.com>
2024-10-17 17:32:42 -0300

    Removendo visão lista da listagem de projetos na Home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,8 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { userInfos } from '../../store/actions/user';
 import { userProjectsInfos } from '../../store/actions/userProjects';
-import { Container, ActionIcon, Box, Avatar, Tooltip, Text, Table, Grid, Paper, Skeleton } from '@mantine/core';
-import { IconStarFilled, IconDots, IconList, IconLayoutGrid } from '@tabler/icons-react';
+import { Container, Avatar, Tooltip, Text, Grid, Paper, Skeleton } from '@mantine/core';
 import Header from '../../components/header';
 import ProjectCard from './projectCard';
 
@@ -18,28 +17,8 @@ function Home () {
     dispatch(userInfos.getUserRolesInfoById(loggedUser.id));
   }, [loggedUser.id, dispatch]);
 
-  const user = useSelector(state => state.user);
+  // const user = useSelector(state => state.user);
   const projects = useSelector(state => state.userProjects);
-  const [layoutDisplay, setLayoutdisplay] = useState('cards');
-
-  const rows = projects?.list.map((p) => (
-    <Table.Tr 
-      key={p.id} 
-      // c={p.yearLeftTheProject ? 'dimmed' : ''}
-    >
-      <Table.Td>{!!p.featured && <IconStarFilled height={10} w={0} />}{p.name}</Table.Td>
-      <Table.Td>{p.ptname} {p.yearEnd && `(encerrado em ${p.yearEnd})`}</Table.Td>
-      <Table.Td>{p.joined_in}</Table.Td>
-      <Table.Td>{p.genre1}</Table.Td>
-      <Table.Td>{p.role1}{p.role2 && ', '+p.role2}{p.role3 && ', '+p.role3}</Table.Td>
-      <Table.Td>{p.workTitle} {p.yearLeftTheProject && `até ${p.yearLeftTheProject}`}</Table.Td>
-      <Table.Td>
-        <ActionIcon variant="transparent" aria-label="Settings">
-          <IconDots style={{ width: '70%', height: '70%' }} stroke={1.5} />
-        </ActionIcon>
-      </Table.Td>
-    </Table.Tr>
-  ));
 
   const projectsTerminated = projects.list.filter((project) => { return project.yearEnd });
 
@@ -49,9 +28,9 @@ function Home () {
       <Container size={'sm'} mb={'md'}>
         {projects.requesting ? (
           <>
-            <Skeleton height={110} radius="xl" />
-            <Skeleton height={58} mt={6} radius="xl" />
-            <Skeleton height={58} mt={6} width="70%" radius="xl" />
+            <Skeleton height={110} radius="md" />
+            <Skeleton height={58} mt={6} radius="md" />
+            <Skeleton height={58} mt={6} width="70%" radius="md" />
           </>
         ) : (
           <>
@@ -60,7 +39,7 @@ function Home () {
                 Você está cadastrado em {projects?.list.length} projetos
               </Text>
               <Avatar.Group>
-                {projects?.list.map((p) => (
+                {projects?.list.slice(0, 5).map((p) => (
                   <Tooltip label={p.name} key={p.id} withArrow>
                     <Avatar 
                       size='lg'
@@ -68,30 +47,31 @@ function Home () {
                     />
                   </Tooltip>
                 ))}
+                <Avatar size='lg'>+{projects?.list.length - 5}</Avatar>
               </Avatar.Group>
             </Paper>
             <Grid grow gutter="xs">
               <Grid.Col span={6}>
                 <Paper shadow="sm" radius="md" withBorder p='sm'>
-                  <Text size="sm">Principais</Text>
+                  <Text size="xs">Principais</Text>
                   <Text size="xs" c="dimmed">4 projetos</Text>
                 </Paper>
               </Grid.Col>
               <Grid.Col span={6}>
                 <Paper shadow="sm" radius="md" withBorder p='sm'>
-                  <Text size="sm">Temporários</Text>
+                  <Text size="xs">Temporários</Text>
                   <Text size="xs" c="dimmed">4 projetos</Text>
                 </Paper>
               </Grid.Col>
               <Grid.Col span={6}>
                 <Paper shadow="sm" radius="md" withBorder p='sm'>
-                  <Text size="sm">Ativo em</Text>
+                  <Text size="xs">Ativo em</Text>
                   <Text size="xs" c="dimmed">4 projetos</Text>
                 </Paper>
               </Grid.Col>
               <Grid.Col span={6}>
                 <Paper shadow="sm" radius="md" withBorder p='sm'>
-                  <Text size="sm">Encerrados</Text>
+                  <Text size="xs">Encerrados</Text>
                   <Text size="xs" c="dimmed">{projectsTerminated?.length} projetos</Text>
                 </Paper>
               </Grid.Col>
@@ -100,62 +80,23 @@ function Home () {
         )}
       </Container>
       <Container size={'lg'}>
-        <Box mb={22}>
-          <ActionIcon 
-            variant={layoutDisplay === "cards" ? "default" : "outline"}
-            // color="violet"
-            onClick={() => setLayoutdisplay('cards')}
-            size="lg"
-            mr={4} 
-          >
-            <IconLayoutGrid style={{ width: '70%', height: '70%' }} stroke={1.5} />
-          </ActionIcon>
-          <ActionIcon 
-            variant={layoutDisplay === "list" ? "default" : "outline"}
-            // color="violet"
-            onClick={() => setLayoutdisplay('list')}
-            size="lg"
-          >
-            <IconList style={{ width: '70%', height: '70%' }} stroke={1.5} />
-          </ActionIcon>
-        </Box>
-        {layoutDisplay === 'list' && 
-          <Table.ScrollContainer minWidth={500} type="native">
-            <Table striped highlightOnHover stickyHeader>
-              <Table.Thead>
-                <Table.Tr>
-                  <Table.Th>Projeto</Table.Th>
-                  <Table.Th>Tipo do projeto</Table.Th>
-                  <Table.Th>Ano entrada</Table.Th>
-                  <Table.Th>Estilo/Gênero</Table.Th>
-                  <Table.Th>Papéis</Table.Th>
-                  <Table.Th>Atuação</Table.Th>
-                  <Table.Th></Table.Th>
-                </Table.Tr>
-              </Table.Thead>
-              <Table.Tbody>{rows}</Table.Tbody>
-            </Table>
-          </Table.ScrollContainer>
-        }
-        {layoutDisplay === 'cards' && 
-          <Grid>
-            {projects?.list.map((p) => (
-              <Grid.Col span={{ base: 12, md: 2, lg: 3 }} key={p.id}>
-                <ProjectCard 
-                  project={p}
-                  activeMembers={
-                    projects?.members?.filter(
-                      (member) => { 
-                        return member.projectId === p.projectid 
-                        && !member.leftIn 
-                      }
-                    ).sort((a, b) => b.leader - a.leader)
-                  }
-                />
-              </Grid.Col>
-            ))}
-          </Grid>
-        }
+        <Grid>
+          {projects?.list.map((p) => (
+            <Grid.Col span={{ base: 12, md: 2, lg: 3 }} key={p.id}>
+              <ProjectCard 
+                project={p}
+                activeMembers={
+                  projects?.members?.filter(
+                    (member) => { 
+                      return member.projectId === p.projectid 
+                      && !member.leftIn 
+                    }
+                  ).sort((a, b) => b.leader - a.leader)
+                }
+              />
+            </Grid.Col>
+          ))}
+        </Grid>
       </Container>
     </>
   );
--- src/pages/Home/projectCard.jsx
+++ src/pages/Home/projectCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Group, Text, Card, Image, Badge, Menu, Avatar, Indicator, ActionIcon, Flex, Tooltip, rem } from '@mantine/core';
+import { Group, Title, Text, Card, Image, Badge, Menu, Avatar, Indicator, ActionIcon, Flex, Tooltip, rem } from '@mantine/core';
 import { IconDots, IconEye, IconFileZip, IconTrash , IconUsersGroup, IconUser, IconBulb, IconFolder, IconIdBadge2, IconSettings, IconUserOff } from '@tabler/icons-react';
 
 function ProjectCard (props) {
@@ -27,189 +27,181 @@ function ProjectCard (props) {
   }
 
   return (
-    <Indicator 
-      // processing={isActiveOnProject}
-      // color={indicatorColor(project)}
-      // position="top-start" 
-      // disabled={!isActiveOnProject}
-      disabled
-    >
-      <Card shadow="sm" padding="lg" radius="md" withBorder>
-        <Card.Section withBorder inheritPadding py="xs">
-          <Group justify="space-between" align="flex-start">
-            <div>
-              <Text fw={500} lineClamp={1}>{project?.name}</Text>
-              <Group gap={4}>
-                {project?.ptname === "Projeto solo" &&
-                  <IconUser style={{ width: rem(13), height: rem(13) }} color='grey' stroke={1.5} /> 
-                }
-                {project?.ptname === "Banda" &&
-                  <IconUsersGroup style={{ width: rem(13), height: rem(13) }} color='grey' stroke={1.5} />
-                } 
-                {project?.ptname === "Ideia de projeto" &&
-                  <IconBulb style={{ width: rem(13), height: rem(13) }} color='grey' stroke={1.5} />
-                }
-                <Text size="xs" c="dimmed" lineClamp={1}>
-                  {project?.ptname} {project.genre1 ? ' · '+project.genre1 : null }
-                </Text>
-              </Group>
-              <Text size="xs" c="dimmed" lineClamp={1} mt={5}>
-                <Badge variant='dot' color={project?.activityStatusColor} size='xs'>
-                  {project?.activityStatus} {(project.activityStatusId === 2 && project.yearEnd) && `em ${project.yearEnd}`}
-                </Badge>
+    <Card shadow="sm" padding="lg" radius="md" withBorder>
+      <Card.Section withBorder inheritPadding py="xs">
+        <Group justify="space-between" align="flex-start">
+          <div>
+            <Title fw={500} lineClamp={1} size="h4">{project?.name}</Title>
+            <Group gap={4}>
+              {project?.ptname === "Projeto solo" &&
+                <IconUser style={{ width: rem(13), height: rem(13) }} color='grey' stroke={1.5} /> 
+              }
+              {project?.ptname === "Banda" &&
+                <IconUsersGroup style={{ width: rem(13), height: rem(13) }} color='grey' stroke={1.5} />
+              } 
+              {project?.ptname === "Ideia de projeto" &&
+                <IconBulb style={{ width: rem(13), height: rem(13) }} color='grey' stroke={1.5} />
+              }
+              <Text size="xs" c="dimmed" lineClamp={1}>
+                {project?.ptname} {project.genre1 ? ' · '+project.genre1 : null }
               </Text>
-            </div>
-            <Menu withinPortal position="bottom-end" shadow="sm">
-              <Menu.Target>
-                <ActionIcon variant="subtle" color="gray">
-                  <IconDots style={{ width: '16px', height: '16px' }} />
-                </ActionIcon>
-              </Menu.Target>
-              <Menu.Dropdown>
-                <Menu.Item leftSection={<IconFileZip style={{ width: '14px', height: '14px' }} />}>
-                  Painel do Projeto
-                </Menu.Item>
-                <Menu.Item leftSection={<IconEye style={{ width: '14px', height: '14px' }} />}>
-                  Perfil do Projeto
-                </Menu.Item>
-                <Menu.Item
-                  leftSection={<IconTrash style={{ width: '14px', height: '14px' }} />}
-                  color="red"
-                >
-                  Gerenciar participação
-                </Menu.Item>
-              </Menu.Dropdown>
-            </Menu>
-          </Group>
-        </Card.Section>
-        <Card.Section>
-          <Image
-            src={cdnProjectPath+project?.picture}
-            height={160}
-            alt="Norway"
-          />
-        </Card.Section>
-        <Card.Section withBorder inheritPadding py="xs">
-          <Text size="xs">
-            Minha participação
-          </Text>
-          {isActiveOnProject && 
-            <Badge size='xs' variant='light' color={indicatorColor()}>
-              {`Ativo desde ${project.joined_in}`}
-            </Badge>
-          }
-          {project.yearLeftTheProject && 
-            <Badge size='xs' variant='light' color="red">
-              deixei o projeto em {project.yearLeftTheProject}
-            </Badge>
-          }
-          {(project.activityStatusId === 2 && project.yearEnd && !project.yearLeftTheProject) && 
-            <Badge size='xs' variant='light' color="red">
-              estive até o encerramento em {project.yearEnd}
-            </Badge>
-          }
-        </Card.Section>
-        <Card.Section withBorder inheritPadding py="xs">
-          <Flex
-            justify="flex-start"
-            align="center"
-            direction="row"
-            wrap="nowrap"
-            columnGap="md"
+            </Group>
+            <Text size="xs" c="dimmed" lineClamp={1} mt={5}>
+              <Badge variant='dot' color={project?.activityStatusColor} size='xs'>
+                {project?.activityStatus} {(project.activityStatusId === 2 && project.yearEnd) && `em ${project.yearEnd}`}
+              </Badge>
+            </Text>
+          </div>
+          <Menu withinPortal position="bottom-end" shadow="sm">
+            <Menu.Target>
+              <ActionIcon variant="subtle" color="gray">
+                <IconDots style={{ width: '16px', height: '16px' }} />
+              </ActionIcon>
+            </Menu.Target>
+            <Menu.Dropdown>
+              <Menu.Item leftSection={<IconFileZip style={{ width: '14px', height: '14px' }} />}>
+                Painel do Projeto
+              </Menu.Item>
+              <Menu.Item leftSection={<IconEye style={{ width: '14px', height: '14px' }} />}>
+                Perfil do Projeto
+              </Menu.Item>
+              <Menu.Item
+                leftSection={<IconTrash style={{ width: '14px', height: '14px' }} />}
+                color="red"
+              >
+                Gerenciar participação
+              </Menu.Item>
+            </Menu.Dropdown>
+          </Menu>
+        </Group>
+      </Card.Section>
+      <Card.Section>
+        <Image
+          src={cdnProjectPath+project?.picture}
+          height={160}
+          alt="Norway"
+        />
+      </Card.Section>
+      <Card.Section withBorder inheritPadding py="xs">
+        <Text size="xs">
+          Minha participação
+        </Text>
+        {isActiveOnProject && 
+          <Badge size='xs' variant='light' color={indicatorColor()}>
+            {`Ativo desde ${project.joined_in}`}
+          </Badge>
+        }
+        {project.yearLeftTheProject && 
+          <Badge size='xs' variant='light' color="red">
+            deixei o projeto em {project.yearLeftTheProject}
+          </Badge>
+        }
+        {(project.activityStatusId === 2 && project.yearEnd && !project.yearLeftTheProject) && 
+          <Badge size='xs' variant='light' color="red">
+            estive até o encerramento em {project.yearEnd}
+          </Badge>
+        }
+      </Card.Section>
+      <Card.Section withBorder inheritPadding py="xs">
+        <Flex
+          justify="flex-start"
+          align="center"
+          direction="row"
+          wrap="nowrap"
+          columnGap="md"
+        >
+          <Indicator 
+            processing={isActiveOnProject}
+            color={indicatorColor(project)}
+            size={8}
+            position="top-end"
+            // display={'inline-table'}
+            // style={{ display: 'inline-table' }}
           >
-            <Indicator 
-              processing={isActiveOnProject}
-              color={indicatorColor(project)}
-              size={8}
-              position="top-end"
-              // display={'inline-table'}
-              // style={{ display: 'inline-table' }}
+            <Avatar 
+              variant="filled" 
+              radius="sm" 
+              size="md" 
+              src={
+                (user.id && user.picture) ? 
+                  cdnBaseURL+'tr:h-66,w-66,r-max,c-maintain_ratio/users/avatars/'+user.id+'/'+user.picture
+                : null
+                } 
+            />
+          </Indicator>
+          <Flex
+            direction="column"
+            wrap="wrap"
+            rowGap={3}
+          >
+            <Text size="xs" lineClamp={1}>
+              {project.role1}{project.role2 && ', '+project.role2}{project.role3 && ', '+project.role3}
+            </Text>
+            <Text size="xs" display={'flex'} lineClamp={1}>
+              <IconIdBadge2 style={{ width: '15px', height: '15px', marginRight: '3px' }} stroke={1.2} /> {project.workTitle}
+            </Text>
+            <Text size="xs" display={'flex'} lineClamp={1}>
+              <IconSettings style={{ width: '15px', height: '15px', marginRight: '3px' }} stroke={1.2} /> Administrador
+            </Text>
+          </Flex>
+        </Flex>
+      </Card.Section>
+      <Card.Section withBorder inheritPadding py="xs">
+        <Text size="xs" c="dimmed" mb={5}>
+          {activeMembers?.length} Integrantes/equipe ativos
+        </Text>
+        <Group gap={5}>
+          {activeMembers?.map((member) => (
+            <Tooltip 
+              label={`${member.userName} ${member.userLastname} - ${member.role1}`} 
+              key={member.userId} 
+              withArrow
             >
               <Avatar 
                 variant="filled" 
                 radius="sm" 
-                size="md" 
+                size="sm" 
                 src={
-                  (user.id && user.picture) ? 
-                    cdnBaseURL+'tr:h-66,w-66,r-max,c-maintain_ratio/users/avatars/'+user.id+'/'+user.picture
+                  (member.userId && member.userPicture) ? 
+                    cdnBaseURL+'tr:h-26,w-26,r-max,c-maintain_ratio/users/avatars/'+member.userId+'/'+member.userPicture
                   : null
-                 } 
+                } 
               />
-            </Indicator>
-            <Flex
-              direction="column"
-              wrap="wrap"
-              rowGap={3}
+            </Tooltip>
+          ))}
+          {!activeMembers?.length && 
+            <Avatar 
+              variant="filled" 
+              radius="xl" 
+              size="sm"
             >
-              <Text size="xs" lineClamp={1}>
-                {project.role1}{project.role2 && ', '+project.role2}{project.role3 && ', '+project.role3}
-              </Text>
-              <Text size="xs" display={'flex'} lineClamp={1}>
-                <IconIdBadge2 style={{ width: '15px', height: '15px', marginRight: '3px' }} stroke={1.2} /> {project.workTitle}
-              </Text>
-              <Text size="xs" display={'flex'} lineClamp={1}>
-                <IconSettings style={{ width: '15px', height: '15px', marginRight: '3px' }} stroke={1.2} /> Administrador
-              </Text>
-            </Flex>
-          </Flex>
-        </Card.Section>
-        <Card.Section withBorder inheritPadding py="xs">
-          <Text size="xs" c="dimmed" mb={5}>
-            {activeMembers?.length} Integrantes/equipe ativos
-          </Text>
-          <Group gap={5}>
-            {activeMembers?.map((member) => (
-              <Tooltip 
-                label={`${member.userName} ${member.userLastname} - ${member.role1}`} 
-                key={member.userId} 
-                withArrow
-              >
-                <Avatar 
-                  variant="filled" 
-                  radius="sm" 
-                  size="sm" 
-                  src={
-                    (member.userId && member.userPicture) ? 
-                      cdnBaseURL+'tr:h-26,w-26,r-max,c-maintain_ratio/users/avatars/'+member.userId+'/'+member.userPicture
-                    : null
-                  } 
-                />
-              </Tooltip>
-            ))}
-            {!activeMembers?.length && 
-              <Avatar 
-                variant="filled" 
-                radius="xl" 
-                size="sm"
-              >
-                <IconUserOff size="1rem" />
-              </Avatar>
-            }
-          </Group>
-        </Card.Section>
-        <Card.Section withBorder inheritPadding py="xs">
-          <Flex align="center" justify="flex-start" direction="row" c="dimmed">
-            <Text style={{ fontSize: '11px' }}>
-              Categoria: 
-            </Text>
-            <IconFolder 
-              style={{ width: '14px', height: '14px', marginLeft: '3px', marginRight: '3px' }}
-            />
-            <Text style={{ fontSize: '11px' }}>
-              {project.portfolio ? 'Portfolio' : 'Principais'}
-            </Text>
-          </Flex>
-        </Card.Section>
-        {/* <Card.Section withBorder inheritPadding py="xs">
-          {!!project.id && 
-            <div>
-              <Badge size="xs" variant="light">{project.workTitle}</Badge>
-              {!!(project.touring && !project.yearLeftTheProject && !project.yearEnd) && <Badge size="xs" variant="light">Em turnê com este projeto</Badge>} {!!project.admin && <Badge size="xs" variant="light">Administrador</Badge>} {!!(!project.yearLeftTheProject && !project.yearEnd && (currentYear - project.joined_in) >= 10) && <Badge size="xs" variant="light">há + de 10 anos ativo no projeto!</Badge>} 
-            </div>
+              <IconUserOff size="1rem" />
+            </Avatar>
           }
-        </Card.Section> */}
-      </Card>
-    </Indicator>
+        </Group>
+      </Card.Section>
+      <Card.Section withBorder inheritPadding py="xs">
+        <Flex align="center" justify="flex-start" direction="row" c="dimmed">
+          <Text style={{ fontSize: '11px' }}>
+            Categoria: 
+          </Text>
+          <IconFolder 
+            style={{ width: '14px', height: '14px', marginLeft: '3px', marginRight: '3px' }}
+          />
+          <Text style={{ fontSize: '11px' }}>
+            {project.portfolio ? 'Portfolio' : 'Principais'}
+          </Text>
+        </Flex>
+      </Card.Section>
+      {/* <Card.Section withBorder inheritPadding py="xs">
+        {!!project.id && 
+          <div>
+            <Badge size="xs" variant="light">{project.workTitle}</Badge>
+            {!!(project.touring && !project.yearLeftTheProject && !project.yearEnd) && <Badge size="xs" variant="light">Em turnê com este projeto</Badge>} {!!project.admin && <Badge size="xs" variant="light">Administrador</Badge>} {!!(!project.yearLeftTheProject && !project.yearEnd && (currentYear - project.joined_in) >= 10) && <Badge size="xs" variant="light">há + de 10 anos ativo no projeto!</Badge>} 
+          </div>
+        }
+      </Card.Section> */}
+    </Card>
   );
 };
 
