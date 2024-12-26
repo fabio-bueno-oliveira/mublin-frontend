f19d8afcb7cb5f521ecc8e8e28a7d4748b14d87a
fabio bueno <fabiobueno@outlook.com>
2024-12-25 22:15:52 -0300

    fix: ajustes na página de cadastro de novo equipamento

--- src/pages/Settings/submitNew.jsx
+++ src/pages/Settings/submitNew.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { miscInfos } from '../../store/actions/misc'
-import { Grid, Container, Modal, Checkbox, Center, Group, Flex, Alert, Loader, Input, Image, NativeSelect, Button, Radio, Text, Breadcrumbs, Anchor, TextInput } from '@mantine/core'
+import { Grid, Container, Modal, Checkbox, Center, Group, Flex, Alert, Loader, Input, Image, NativeSelect, Button, Stack, Text, Breadcrumbs, Anchor, TextInput } from '@mantine/core'
 import { IconCloudUp, IconPlus } from '@tabler/icons-react'
 import { useMediaQuery } from '@mantine/hooks'
 import Header from '../../components/header'
@@ -138,55 +138,55 @@ function SettingsNewProductPage () {
   }
 
   const submitNewProduct = () => {
-      setIsLoading(true)
-      setTimeout(() => {
-          fetch('https://mublin.herokuapp.com/gear/submitNewGearProduct', {
-              method: 'post',
-              headers: {
-                  'Accept': 'application/json, text/plain, */*',
-                  'Content-Type': 'application/json',
-                  'Authorization': 'Bearer ' + user.token
-              },
-              body: JSON.stringify({name: name, id_brand: brandId, id_category: categoryId, year: null, color: colorId, picture: picture, id_user_creator: user.id})
-          }).then(res => res.json()).then((result) => {
-              setIsLoading(false)
-              setName(null)
-              setBrandId(null)
-              setCategoryId(null)
-              setPicture(null)
-              if (addNewProductToMyGear) {
-                  addProductToMyGear(result.id)
-              } else {
-                  history.push("/career/my-gear")
-              }
-          }).catch(err => {
-              console.error(err)
-              alert("Ocorreu um erro ao adicionar o produto. Talvez o mesmo produto já esteja cadastrado.")
-              setIsLoading(false)
-          })
-      }, 300);
+    setIsLoading(true)
+    setTimeout(() => {
+      fetch('https://mublin.herokuapp.com/gear/submitNewGearProduct', {
+        method: 'post',
+        headers: {
+          'Accept': 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + loggedUser.token
+        },
+        body: JSON.stringify({name: name, id_brand: brandId, id_category: categoryId, year: null, color: colorId, picture: picture, id_user_creator: loggedUser.id})
+      }).then(res => res.json()).then((result) => {
+        setIsLoading(false)
+        setName(null)
+        setBrandId(null)
+        setCategoryId(null)
+        setPicture(null)
+        if (addNewProductToMyGear) {
+          addProductToMyGear(result.id)
+        } else {
+          history.push('/career/my-gear')
+        }
+      }).catch(err => {
+        console.error(err)
+        alert('Ocorreu um erro ao adicionar o produto. Talvez o mesmo produto já esteja cadastrado.')
+        setIsLoading(false)
+      })
+    }, 300);
   }
 
   const addProductToMyGear = (newProductId) => {
-      setIsLoading(true)
-      setTimeout(() => {
-          fetch('https://mublin.herokuapp.com/user/addGearItem', {
-              method: 'post',
-              headers: {
-                  'Accept': 'application/json, text/plain, */*',
-                  'Content-Type': 'application/json',
-                  'Authorization': 'Bearer ' + user.token
-              },
-              body: JSON.stringify({productId: newProductId, featured: 0, for_sale: 0, price: null, currently_using: 1})
-          }).then((response) => {
-              setIsLoading(false)
-              history.push("/career/my-gear")
-          }).catch(err => {
-              console.error(err)
-              alert("Ocorreu um erro ao adicionar o produto à sua lista de equipamentos")
-              setIsLoading(false)
-          })
-      }, 300);
+    setIsLoading(true)
+    setTimeout(() => {
+      fetch('https://mublin.herokuapp.com/user/addGearItem', {
+        method: 'post',
+        headers: {
+          'Accept': 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + loggedUser.token
+        },
+        body: JSON.stringify({productId: newProductId, featured: 0, for_sale: 0, price: null, currently_using: 1})
+      }).then((response) => {
+        setIsLoading(false)
+        history.push('/career/my-gear')
+      }).catch(err => {
+        console.error(err)
+        alert('Ocorreu um erro ao adicionar o produto à sua lista de equipamentos')
+        setIsLoading(false)
+      })
+    }, 300);
   }
 
   return (
@@ -240,87 +240,95 @@ function SettingsNewProductPage () {
                     Adicionar nova marca
                   </Button>
                 </Flex>
-                <NativeSelect
-                  withAsterisk
-                  label='Categoria'
-                  onChange={(e) => setCategoryId(e.target.options[e.target.selectedIndex].value)}
-                  value={categoryId}
-                  defaultValue={categoryId}
+                <Stack
+                  align="stretch"
+                  justify="flex-start"
+                  gap="md"
+                  mb={310}
                 >
-                  {(!isCategoriesLoaded) && 
-                    <option>Carregando...</option>
-                  }
-                  { !categoryId && 
-                    <option>Selecione</option>
-                  }
-                  { macroCategories.map((macroCategory,key) =>
-                    <>
-                      <optgroup label={macroCategory.name}>
-                        { categories.filter((x) => { return x.macro_category === macroCategory.name }).map((category,key) =>
-                          <option key={key} value={category.id}>{category.name}</option>
-                        )}
-                      </optgroup>
-                    </>
-                  )}
-                </NativeSelect>
-                <TextInput
-                  label='Modelo (sem a marca)'
-                  placeholder='Ex: BD-2 Blues Driver'
-                  value={name}
-                  onChange={(event) => setName(event.currentTarget.value)}
-                />
-                <NativeSelect
-                  withAsterisk
-                  label='Cor do produto'
-                  onChange={(e) => setColorId(e.target.options[e.target.selectedIndex].value)}
-                  value={colorId}
-                  defaultValue={colorId}
-                >
-                  {(!isColorsLoaded) && 
-                    <option>Carregando...</option>
-                  }
-                  { !colorId && 
-                    <option>Selecione</option>
-                  }
-                  { colors.map((color,key) =>
-                    <option key={key} value={color.id}>{color.name} {color.name_ptbr && '('+color.name_ptbr+')'}</option>
-                  )}
-                </NativeSelect>
-                <Checkbox
-                  label='Adicionar este produto ao Meu Equipamento'
-                  checked={addNewProductToMyGear ? true : false}
-                  // onChange={(event) => setChecked(event.currentTarget.checked)}
-                  onChange={() => setAddNewProductToMyGear(value => !value)}
-                />
-                <Input.Label required>Foto genérica do produto</Input.Label>
-                <Alert mb={16} variant="light" color="yellow">
-                  Envie um arquivo de até 2mb. A foto do produto deve ser genérica e com qualidade, de preferência em estado de novo. A foto deve ter aproximadamente 22 pixels de margem em cada extremidade (independente da largura ou altura), e fundo branco ou transparente. De prefência, de frente sem inclinação.<strong>Uma vez cadastrado, o produto não poderá ter a foto substituída!</strong>
-                </Alert>
-                <Center>
-                  <Image src='https://ik.imagekit.io/mublin/misc/product-image-upload-guide_SRC738Rba.png' height='215' />
-                  {picture && 
-                    <Image src={'https://ik.imagekit.io/mublin/tr:h-300,cm-pad_resize,bg-FFFFFF/products/'+picture} height='205' />
-                  }
-                </Center>
-                <div className={picture ? 'd-none' : ''}>
-                  <IKUpload 
-                    id='picture'
-                    fileName="avatar.jpg"
-                    folder={imageUploadPath}
-                    tags={["gear"]}
-                    useUniqueFileName={true}
-                    isPrivateFile= {false}
-                    onError={onUploadError}
-                    onSuccess={onUploadSuccess}
-                    accept="image/x-png,image/gif,image/jpeg" 
+                  <NativeSelect
+                    withAsterisk
+                    label='Categoria'
+                    onChange={(e) => setCategoryId(e.target.options[e.target.selectedIndex].value)}
+                    value={categoryId}
+                    defaultValue={categoryId}
+                  >
+                    {(!isCategoriesLoaded) && 
+                      <option>Carregando...</option>
+                    }
+                    { !categoryId && 
+                      <option>Selecione</option>
+                    }
+                    { macroCategories.map((macroCategory,key) =>
+                      <>
+                        <optgroup label={macroCategory.name}>
+                          { categories.filter((x) => { return x.macro_category === macroCategory.name }).map((category,key) =>
+                            <option key={key} value={category.id}>{category.name}</option>
+                          )}
+                        </optgroup>
+                      </>
+                    )}
+                  </NativeSelect>
+                  <TextInput
+                    label='Modelo (sem a marca)'
+                    placeholder='Ex: BD-2 Blues Driver'
+                    value={name}
+                    onChange={(event) => setName(event.currentTarget.value)}
                   />
-                </div>
-                <Button
-                  disabled={(!name || !picture || !brandId || !categoryId) ? true : false}
-                  onClick={() => submitNewProduct()}
-                >
-                  Enviar
-                </Button>
+                  <NativeSelect
+                    withAsterisk
+                    label='Cor do produto'
+                    onChange={(e) => setColorId(e.target.options[e.target.selectedIndex].value)}
+                    value={colorId}
+                    defaultValue={colorId}
+                  >
+                    {(!isColorsLoaded) && 
+                      <option>Carregando...</option>
+                    }
+                    { !colorId && 
+                      <option>Selecione</option>
+                    }
+                    { colors.map((color,key) =>
+                      <option key={key} value={color.id}>{color.name} {color.name_ptbr && '('+color.name_ptbr+')'}</option>
+                    )}
+                  </NativeSelect>
+                  <Checkbox
+                    label='Adicionar este produto ao Meu Equipamento'
+                    checked={addNewProductToMyGear ? true : false}
+                    // onChange={(event) => setChecked(event.currentTarget.checked)}
+                    onChange={() => setAddNewProductToMyGear(value => !value)}
+                  />
+                  <Input.Label required>Foto genérica do produto</Input.Label>
+                  <Alert mb={16} variant="light" color="gray">
+                    Envie um arquivo de até 2mb. A foto do produto deve ser genérica e com qualidade, de preferência em estado de novo. A foto deve ter aproximadamente 22 pixels de margem em cada extremidade (independente da largura ou altura), e fundo branco ou transparente. De prefência, de frente sem inclinação.
+                  </Alert>
+                  <Center>
+                    <Image src='https://ik.imagekit.io/mublin/misc/product-image-upload-guide_SRC738Rba.png' height='215' />
+                    {picture && 
+                      <Image src={'https://ik.imagekit.io/mublin/tr:h-300,cm-pad_resize,bg-FFFFFF/products/'+picture} height='205' />
+                    }
+                  </Center>
+                  <div className={picture ? 'd-none' : ''}>
+                    <IKUpload 
+                      id='picture'
+                      fileName="avatar.jpg"
+                      folder={imageUploadPath}
+                      tags={["gear"]}
+                      useUniqueFileName={true}
+                      isPrivateFile= {false}
+                      onError={onUploadError}
+                      onSuccess={onUploadSuccess}
+                      accept="image/x-png,image/gif,image/jpeg" 
+                    />
+                  </div>
+                  <Button
+                    mt={10}
+                    disabled={(!name || !picture || !brandId || !categoryId) ? true : false}
+                    onClick={() => submitNewProduct()}
+                  >
+                    Enviar
+                  </Button>
+                </Stack>
               </>
             )}
           </Grid.Col>
@@ -331,16 +339,49 @@ function SettingsNewProductPage () {
         opened={modalNewBrandOpen}
         onClose={() => setModalNewBrandOpen(false)}
         size='xs'
+        title='Cadastrar nova marca de equipamento'
         centered
       >
-        <Text>
-          Teste
-        </Text>
+        <Alert mb={16} variant="light" color="yellow">
+          Evite duplicidade! Certifique-se que a marca já não esteja cadastrada
+        </Alert>
+        <TextInput
+          label='Nome'
+          placeholder='Ex: Fender'
+          value={newBrandName}
+          onChange={(event) => setNewBrandName(event.currentTarget.value)}
+        />
+        <Input.Label for='newBrandLogo'>Logotipo</Input.Label>
+        <div className={picture ? 'd-none' : ''}>
+          <IKUpload
+            id='newBrandLogo'
+            fileName={newBrandName+'_avatar.jpg'}
+            folder={logoUploadPath}
+            tags={['logo']}
+            useUniqueFileName={true}
+            isPrivateFile= {false}
+            onError={onLogoUploadError}
+            onSuccess={onLogoUploadSuccess}
+            accept='image/x-png,image/gif,image/jpeg'
+            style={{fontSize:'12px'}}
+          />
+        </div>
         <Group justify='flex-end' gap={7} mt={20}>
-          <Button variant='outline' color='gray' size='sm'>
+          <Button 
+            variant='outline' 
+            color='gray' 
+            size='sm' 
+            onClick={() => setModalNewBrandOpen(false)}
+          >
             Cancelar
           </Button>
-          <Button color='violet' size='sm'>
+          <Button 
+            color='violet' 
+            size='sm' 
+            disabled={(!newBrandName || !newBrandLogo) ? true : false} 
+            onClick={() => submitNewBrand()} 
+            loading={isLoading}
+          >
             Cadastrar
           </Button>
         </Group>
