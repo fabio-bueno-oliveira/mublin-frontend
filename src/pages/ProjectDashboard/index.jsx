382b8ea97088bd41f5bae4688a7900eb692ee122
fabio bueno <fabiobueno@outlook.com>
2025-03-28 22:24:25 -0300

    fix: ajustes menu dashboard projeto

--- src/pages/ProjectDashboard/index.jsx
+++ src/pages/ProjectDashboard/index.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react'
-import { Link } from 'react-router-dom'
 import { useParams } from 'react-router'
 import { projectInfos } from '../../store/actions/project'
 import { useDispatch, useSelector } from 'react-redux'
-import { useMantineColorScheme, Grid, Menu, Box, Center, Flex, Title, Button, Card, Text, Image, Skeleton, Avatar, NavLink, Anchor, Loader, ThemeIcon } from '@mantine/core'
-import { IconLayoutDashboard, IconUsersGroup, IconCalendarPlus, IconCalendarWeek, IconArrowLeft, IconPhoto, IconPictureInPictureTop, IconPencil, IconCalendar, IconMusicPlus, IconList, IconTrash, IconArrowsLeftRight, IconSearch, IconMessageCircle, IconSettings, IconMusic, IconUserPlus, IconPlus } from '@tabler/icons-react'
+import { useMantineColorScheme, Grid, Group, Menu, Box, Center, Flex, Title, Button, Text, Image, Skeleton, Avatar, Loader, ThemeIcon, Indicator, Divider, Drawer, Burger, rem, em } from '@mantine/core'
+import { useDisclosure, useMediaQuery } from '@mantine/hooks'
+import { IconPhoto, IconCalendar, IconTrash, IconArrowsLeftRight, IconSearch, IconMessageCircle, IconSettings, IconMusic, IconMoon, IconBrightnessUp, IconMenu2 } from '@tabler/icons-react'
+import Navbar from './navbar'
 import MublinLogoBlack from '../../assets/svg/mublin-logo.svg'
 import MublinLogoWhite from '../../assets/svg/mublin-logo-w.svg'
 import dayjs from 'dayjs'
@@ -24,8 +25,9 @@ function ProjectDashboardPage () {
   }, []);
 
   const { colorScheme, setColorScheme } = useMantineColorScheme()
-  const userInfo = JSON.parse(localStorage.getItem('userInfo'))
+  const isMobile = useMediaQuery(`(max-width: ${em(750)})`)
 
+  const userInfo = JSON.parse(localStorage.getItem('userInfo'))
   const project = useSelector(state => state.project);
 
   const activeMembers = project.members.filter(
@@ -40,146 +42,106 @@ function ProjectDashboardPage () {
 
   const currentDate = dayjs().format('DD/MM/YYYY')
 
+  const [opened, { open, close }] = useDisclosure(false)
+
   return (
     <>
+      <Drawer opened={opened} onClose={close} size="xs">
+        <Navbar mobile />
+      </Drawer>
       <Grid id='dashboard' gutter={0}>
         <Grid.Col span={{ base: 12, md: 2.5, lg: 2.5 }} id='desktopSidebar' p={14}>
-          <Image
-            radius='md'
-            h={30}
-            w='auto'
-            fit='contain'
-            src={colorScheme === 'light' ? MublinLogoBlack : MublinLogoWhite} 
-            mt={16}
-            mb={6}
-            ml={8}
-          />
-          <Anchor c='dimmed' href='/home' underline="hover" fz='xs' ml={10}>
-            Voltar ao Mublin
-          </Anchor>
-          <NavLink
-            mt={16}
-            active
-            href={`/dashboard/${username}`}
-            label='Início'
-            leftSection={<IconLayoutDashboard size={16} stroke={1.5} />}
-            color='mublinColor'
-          />
-          <Text ml={10} my={8} c='dimmed' size='xs' tt='uppercase'>Dados do projeto</Text>
-          <NavLink
-            href='#required-for-focus'
-            label='Informações'
-            leftSection={<IconPencil size={16} stroke={1.5} />}
-            color='mublinColor'
-          />
-          <NavLink
-            href='#required-for-focus'
-            label='Foto de perfil'
-            leftSection={<IconPhoto size={16} stroke={1.5} />}
-            color='mublinColor'
-          />
-          <NavLink
-            href='#required-for-focus'
-            label='Foto de capa'
-            leftSection={<IconPictureInPictureTop size={16} stroke={1.5} />}
-            color='mublinColor'
-          />
-          <Text ml={10} my={8} c='dimmed' size='xs' tt='uppercase'>Pessoas e funções</Text>
-          <NavLink
-            href='#required-for-focus'
-            label='Gerenciar pessoas'
-            leftSection={<IconUsersGroup size={16} stroke={1.5} />}
-            color='mublinColor'
-          />
-          <Text ml={10} my={8} c='dimmed' size='xs' tt='uppercase'>Vagas e oportunidades</Text>
-          <NavLink
-            href='#required-for-focus'
-            label='Criar nova vaga'
-            leftSection={<IconPlus size={16} stroke={1.5} />}
-            color='mublinColor'
-          />
-          <Text ml={10} my={8} c='dimmed' size='xs' tt='uppercase'>Eventos</Text>
-          <NavLink
-            href='#required-for-focus'
-            label='Criar novo evento'
-            leftSection={<IconCalendarPlus size={16} stroke={1.5} />}
-            color='mublinColor'
-          />
-          <NavLink
-            href='#required-for-focus'
-            label='Ver eventos'
-            leftSection={<IconCalendarWeek size={16} stroke={1.5} />}
-            color='mublinColor'
-          />
-          <Text ml={10} my={8} c='dimmed' size='xs' tt='uppercase'>Músicas</Text>
-          <NavLink
-            href='#required-for-focus'
-            label='Cadastrar nova música'
-            leftSection={<IconMusicPlus size={16} stroke={1.5} />}
-            color='mublinColor'
-          />
-          <NavLink
-            href='#required-for-focus'
-            label='Ver músicas'
-            leftSection={<IconList size={16} stroke={1.5} />}
-            color='mublinColor'
-          />
+          <Group align='center' gap={6} className='showOnlyInMobile'>
+            <Image
+              radius='md'
+              h={30}
+              w='auto'
+              fit='contain'
+              src={colorScheme === 'light' ? MublinLogoBlack : MublinLogoWhite} 
+              ml={6}
+            />
+            <IconMenu2 size={26} onClick={open} />
+          </Group>
+          <Box className='showOnlyInLargeScreen'>
+            <Navbar desktop />
+          </Box>
         </Grid.Col>
         <Grid.Col span={{ base: 12, md: 9.5, lg: 9.5 }} pl={30} pr={50} py={30}>
-          {/* <Button size='xs' color='primary' variant='outline' leftSection={<IconArrowLeft size={14} />}>
-            Voltar ao Mublin
-          </Button> */}
-          <Flex justify='space-between'>
-            <Text size='sm' c='dimmed' mb={20}>Painel de Controle / Início</Text>
-            <Menu shadow='md' width={200} position='bottom-end'>
-              <Menu.Target>
-                <Avatar
-                  w={38}
-                  h={38}
-                  className='point'
-                  src={userInfo.picture ? 'https://ik.imagekit.io/mublin/tr:h-76,w-76,r-max,c-maintain_ratio/users/avatars/'+userInfo.id+'/'+userInfo.picture : undefined}
-                  alt={userInfo.username}
-                  ml={8}
-                />
-              </Menu.Target>
-              <Menu.Dropdown>
-                <Menu.Label>Application</Menu.Label>
-                <Menu.Item leftSection={<IconSettings size={14} />}>
-                  Settings
-                </Menu.Item>
-                <Menu.Item leftSection={<IconMessageCircle size={14} />}>
-                  Messages
-                </Menu.Item>
-                <Menu.Item leftSection={<IconPhoto size={14} />}>
-                  Gallery
-                </Menu.Item>
-                <Menu.Item
-                  leftSection={<IconSearch size={14} />}
-                  rightSection={
-                    <Text size="xs" c="dimmed">
-                      ⌘K
-                    </Text>
-                  }
-                >
-                  Search
-                </Menu.Item>
+          <Flex justify='space-between' mb={26}>
+            <Text size='sm' c='dimmed' mb={20}>Painel do projeto / Início</Text>
+            <Group gap={8}>
+              {colorScheme === 'dark' && 
+                  <Button
+                    size='sm'
+                    color='primary'
+                    variant='filled'
+                    leftSection={<IconBrightnessUp style={{ width: rem(14), height: rem(14) }} />}
+                    onClick={() => {setColorScheme('light')}}
+                  >
+                    Tema claro
+                  </Button>
+                }
+                {colorScheme === 'light' && 
+                  <Button
+                    size='sm'
+                    color='primary'
+                    variant='filled'
+                    leftSection={<IconMoon style={{ width: rem(14), height: rem(14) }} />}
+                    onClick={() => {setColorScheme('dark')}}
+                  >
+                    Tema escuro
+                  </Button>
+                }
+              <Menu shadow='md' width={200} position='bottom-end'>
+                <Menu.Target>
+                  <Avatar
+                    w={34}
+                    h={34}
+                    className='point'
+                    src={userInfo.picture ? 'https://ik.imagekit.io/mublin/tr:h-68,w-68,r-max,c-maintain_ratio/users/avatars/'+userInfo.id+'/'+userInfo.picture : undefined}
+                    alt={userInfo.username}
+                    ml={8}
+                  />
+                </Menu.Target>
+                <Menu.Dropdown>
+                  <Menu.Label>Application</Menu.Label>
+                  <Menu.Item leftSection={<IconSettings size={14} />}>
+                    Settings
+                  </Menu.Item>
+                  <Menu.Item leftSection={<IconMessageCircle size={14} />}>
+                    Messages
+                  </Menu.Item>
+                  <Menu.Item leftSection={<IconPhoto size={14} />}>
+                    Gallery
+                  </Menu.Item>
+                  <Menu.Item
+                    leftSection={<IconSearch size={14} />}
+                    rightSection={
+                      <Text size="xs" c="dimmed">
+                        ⌘K
+                      </Text>
+                    }
+                  >
+                    Search
+                  </Menu.Item>
 
-                <Menu.Divider />
+                  <Menu.Divider />
 
-                <Menu.Label>Danger zone</Menu.Label>
-                <Menu.Item
-                  leftSection={<IconArrowsLeftRight size={14} />}
-                >
-                  Transfer my data
-                </Menu.Item>
-                <Menu.Item
-                  color="red"
-                  leftSection={<IconTrash size={14} />}
-                >
-                  Delete my account
-                </Menu.Item>
-              </Menu.Dropdown>
-            </Menu>
+                  <Menu.Label>Danger zone</Menu.Label>
+                  <Menu.Item
+                    leftSection={<IconArrowsLeftRight size={14} />}
+                  >
+                    Transfer my data
+                  </Menu.Item>
+                  <Menu.Item
+                    color="red"
+                    leftSection={<IconTrash size={14} />}
+                  >
+                    Delete my account
+                  </Menu.Item>
+                </Menu.Dropdown>
+              </Menu>
+            </Group>
           </Flex>
           {project.requesting ? (
             <>
@@ -191,44 +153,99 @@ function ProjectDashboardPage () {
             </>
           ) : (
             <>
-              <Title fz='xl'>Dashboard de {project.name}</Title>
-              <Text size='sm'>Informações sobre o projeto</Text>
-              <Text>{currentDate}</Text>
+              <Flex justify='space-between'>
+                <Box>
+                  <Title fz='xl'>Dashboard de {project.name}</Title>
+                  <Text size='sm'>Informações sobre o projeto</Text>
+                </Box>
+                <Button 
+                  size='sm' 
+                  color='primary' 
+                  variant='outline' 
+                  leftSection={<IconCalendar size={14} />}
+                  disabled
+                >
+                  {currentDate}
+                </Button>
+              </Flex>
               <Grid mt={34}>
                 <Grid.Col span={{ base: 12, md: 6, lg: 6 }}>
                   <Grid>
+                    <Grid.Col span={{ base: 12, md: 12, lg: 12 }}>
+                      <Text size='sm' mb={6} fw={600}>Status do projeto:</Text>
+                      {project.activityStatusId &&
+                        <>
+                          <Flex
+                            align='center'
+                            justify='flex-start'
+                            gap={6}
+                          >
+                            <Indicator
+                              inline
+                              processing={project.activityStatusId === 1 || project.activityStatusId === 3}
+                              color={project.activityStatusColor}
+                              size={7}
+                              ml={5}
+                              mr={4}
+                            />
+                            <Text
+                              size='sm'
+                              className='lhNormal'
+                              pt='1px'
+                            >
+                              {project.activityStatus}
+                            </Text>
+                          </Flex>
+                          <Divider mt={16} />
+                        </>
+                      }
+                    </Grid.Col>
                     <Grid.Col span={{ base: 12, md: 6, lg: 6 }}>
-                      <ThemeIcon size='lg' radius='md' variant='light' color='mublinColor'>
-                        <IconCalendar style={{ width: '70%', height: '70%' }} />
-                      </ThemeIcon>
-                      <Title>3</Title>
+                      <Group gap={6}>
+                        <ThemeIcon size='lg' radius='md' variant='filled' color='mublinColor'>
+                          <IconCalendar style={{ width: '70%', height: '70%' }} />
+                        </ThemeIcon>
+                        <Title>3</Title>
+                      </Group>
                       <Text size='sm'>Eventos próximos</Text>
                       <Text size='xs' c='dimmed'>19 no total</Text>
                     </Grid.Col>
                     <Grid.Col span={{ base: 12, md: 6, lg: 6 }}>
-                      <ThemeIcon size='lg' radius='md' variant='light' color='mublinColor'>
-                        <IconMusic style={{ width: '70%', height: '70%' }} />
-                      </ThemeIcon>
-                      <Title>1</Title>
+                      <Group gap={6}>
+                        <ThemeIcon size='lg' radius='md' variant='filled' color='mublinColor'>
+                          <IconMusic style={{ width: '70%', height: '70%' }} />
+                        </ThemeIcon>
+                        <Title>1</Title>
+                      </Group>
                       <Text size='sm'>Músicas</Text>
                     </Grid.Col>
                     <Grid.Col span={{ base: 12, md: 6, lg: 6 }}>
-                      <ThemeIcon size='lg' radius='md' variant='light' color='mublinColor'>
-                        <IconMusic style={{ width: '70%', height: '70%' }} />
-                      </ThemeIcon>
-                      <Title>14</Title>
-                      <Text size='sm'>Eventos cadastrados</Text>
+                      <Group gap={6}>
+                        <ThemeIcon size='lg' radius='md' variant='filled' color='mublinColor'>
+                          <IconMusic style={{ width: '70%', height: '70%' }} />
+                        </ThemeIcon>
+                        <Title>{activeMembers.length}</Title>
+                      </Group>
+                      <Text size='sm'>Integrantes ativos no projeto</Text>
                     </Grid.Col>
                     <Grid.Col span={{ base: 12, md: 6, lg: 6 }}>
-                      <ThemeIcon size='lg' radius='md' variant='light' color='mublinColor'>
-                        <IconCalendar style={{ width: '70%', height: '70%' }} />
-                      </ThemeIcon>
-                      <Title>14</Title>
+                      <Group gap={6}>
+                        <ThemeIcon size='lg' radius='md' variant='filled' color='mublinColor'>
+                          <IconCalendar style={{ width: '70%', height: '70%' }} />
+                        </ThemeIcon>
+                        <Title>14</Title>
+                      </Group>
                       <Text size='sm'>Eventos cadastrados</Text>
+                    </Grid.Col>
+                    <Grid.Col span={{ base: 12, md: 12, lg: 12 }}>
+                      <Text size='sm' mb={6} fw={600}>Recados:</Text>
+                      <Text size='sm' c='dimmed'>
+                        Nenhum recado no momento
+                      </Text>
                     </Grid.Col>
                   </Grid>
                 </Grid.Col>
-                <Grid.Col span={{ base: 12, md: 6, lg: 6 }} pr={100}>
+                <Grid.Col span={{ base: 12, md: 6, lg: 6 }} pr={isMobile ? 0 : 100}>
                   <Box>
                     <Image
                       radius='lg'
@@ -245,17 +262,20 @@ function ProjectDashboardPage () {
                   <Text mb={6} size='sm'>
                     Ano de fundação: <Text span c='dimmed'>{project.foundationYear}</Text> {!!project.endDate && <Text span c='dimmed'>(encerrado em {project.endDate})</Text>}
                   </Text>
+                  <Text mb={6} size='sm'>
+                    Propósito: <Text span c='dimmed'>{project.purpose}</Text>
+                  </Text>
                   <Text size='sm' my={10}>
-                    {project.bio}
+                    Bio: <Text span c='dimmed'>{project.bio}</Text>
                   </Text>
                   <Avatar.Group spacing={10} mt={6}>
                     {activeMembers.map(member =>
                       <Avatar
                         key={member.id}
-                        size='30'
+                        size='45'
                         name={member.name}
                         title={`${member.name} ${member.lastname} - ${member.role1}`}
-                        src={'https://ik.imagekit.io/mublin/users/avatars/tr:h-100,w-100,c-maintain_ratio/'+member.id+'/'+member.picture} 
+                        src={'https://ik.imagekit.io/mublin/users/avatars/tr:h-82,w-82,c-maintain_ratio/'+member.id+'/'+member.picture} 
                         component='a'
                         href={`/${member.username}`}
                       />
---
+++ src/pages/ProjectDashboard/navbar.jsx
@@ -0,0 +1,122 @@
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router'
+import { projectInfos } from '../../store/actions/project'
+import { useDispatch } from 'react-redux'
+import { useMantineColorScheme, Group, Text, Image, Badge, NavLink, Anchor } from '@mantine/core'
+import { IconLayoutDashboard, IconUsersGroup, IconCalendarPlus, IconCalendarWeek, IconArrowLeft, IconPhoto, IconPictureInPictureTop, IconPencil, IconMusicPlus, IconList,  IconReportMoney, IconPlus } from '@tabler/icons-react'
+import MublinLogoBlack from '../../assets/svg/mublin-logo.svg'
+import MublinLogoWhite from '../../assets/svg/mublin-logo-w.svg'
+
+function ProjectDashboardMenu (props) {
+
+  const params = useParams()
+  const username = params?.username
+
+  let dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(projectInfos.getProjectInfo(username))
+    dispatch(projectInfos.getProjectMembers(username))
+  }, [])
+
+  const { colorScheme } = useMantineColorScheme()
+
+  return (
+    <>
+      {!!props.desktop &&
+        <Image
+          radius='md'
+          h={30}
+          w='auto'
+          fit='contain'
+          src={colorScheme === 'light' ? MublinLogoBlack : MublinLogoWhite} 
+          mt={16}
+          ml={6}
+        />
+      }
+      <Group c='dimmed' gap={3} align='center' ml={10} mt={10}>
+        <IconArrowLeft size={13} />
+        <Anchor c='dimmed' href='/home' underline='never'>
+          <Text fz='sm' c='dimmed'>Voltar ao Mublin</Text>
+        </Anchor>
+      </Group>
+      <NavLink
+        mt={16}
+        active
+        href={`/dashboard/${username}`}
+        label='Início'
+        leftSection={<IconLayoutDashboard size={16} stroke={1.5} />}
+        color='mublinColor'
+      />
+      <Text ml={10} my={8} c='dimmed' size='xs' tt='uppercase'>Dados do projeto</Text>
+      <NavLink
+        href='#required-for-focus'
+        label='Informações'
+        leftSection={<IconPencil size={16} stroke={1.5} />}
+        color='mublinColor'
+      />
+      <NavLink
+        href='#required-for-focus'
+        label='Foto de perfil'
+        leftSection={<IconPhoto size={16} stroke={1.5} />}
+        color='mublinColor'
+      />
+      <NavLink
+        href='#required-for-focus'
+        label='Foto de capa'
+        leftSection={<IconPictureInPictureTop size={16} stroke={1.5} />}
+        color='mublinColor'
+      />
+      <Text ml={10} my={8} c='dimmed' size='xs' tt='uppercase'>Pessoas e funções</Text>
+      <NavLink
+        href='#required-for-focus'
+        label='Gerenciar pessoas'
+        leftSection={<IconUsersGroup size={16} stroke={1.5} />}
+        color='mublinColor'
+      />
+      <Text ml={10} my={8} c='dimmed' size='xs' tt='uppercase'>Vagas e oportunidades</Text>
+      <NavLink
+        href='#required-for-focus'
+        label='Criar nova vaga'
+        leftSection={<IconPlus size={16} stroke={1.5} />}
+        color='mublinColor'
+      />
+      <Text ml={10} my={8} c='dimmed' size='xs' tt='uppercase'>Eventos</Text>
+      <NavLink
+        href='#required-for-focus'
+        label='Criar novo evento'
+        leftSection={<IconCalendarPlus size={16} stroke={1.5} />}
+        color='mublinColor'
+      />
+      <NavLink
+        href='#required-for-focus'
+        label='Ver eventos'
+        leftSection={<IconCalendarWeek size={16} stroke={1.5} />}
+        color='mublinColor'
+      />
+      <Text ml={10} my={8} c='dimmed' size='xs' tt='uppercase'>Músicas</Text>
+      <NavLink
+        href='#required-for-focus'
+        label='Cadastrar nova música'
+        leftSection={<IconMusicPlus size={16} stroke={1.5} />}
+        color='mublinColor'
+      />
+      <NavLink
+        href='#required-for-focus'
+        label='Ver músicas'
+        leftSection={<IconList size={16} stroke={1.5} />}
+        color='mublinColor'
+      />
+      <Text ml={10} my={8} c='dimmed' size='xs' tt='uppercase'>Financeiro</Text>
+      <NavLink
+        href='#required-for-focus'
+        label='Gerenciar finanças'
+        leftSection={<IconReportMoney size={16} stroke={1.5} />}
+        rightSection={<Badge size='xs' variant='light' color='mublinColor'>Em breve</Badge>}
+        color='mublinColor'
+      />
+    </>
+  )
+}
+
+export default ProjectDashboardMenu
