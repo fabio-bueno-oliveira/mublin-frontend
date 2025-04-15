ea47aa2c92cd661acfb52992314a9cde01dc0791
fabio bueno <fabiobueno@outlook.com>
2025-04-15 10:44:37 -0300

    fix: melhorias no footer da landing page

--- src/components/footer/public/index.jsx
+++ src/components/footer/public/index.jsx
@@ -1,26 +1,54 @@
-import { Flex, Text, ActionIcon, rem } from '@mantine/core'
+import { Grid, Flex, Box, Text, ActionIcon, Image, rem, em } from '@mantine/core'
+import { useMediaQuery } from '@mantine/hooks'
+import { Link } from 'react-router-dom'
 import { IconBrandInstagram } from '@tabler/icons-react'
+import MublinLogoWhite from '../../../assets/svg/mublin-logo-w.svg'
+import PianoLogoWhite from '../../../assets/svg/piano-logo-w.svg'
 
 function Footer () {
 
+  const isMobile = useMediaQuery(`(max-width: ${em(750)})`)
+
   return (
-    <Flex gap={10} mt={20} mb={30} justify='center' align='center'>
-      <Text ta='center' c='dimmed' size='xs'>
+    <Box 
+      bg='black'
+      px={isMobile ? 20 : 120} 
+      py={20} 
+      mt={20}
+      h={200}
+    >
+      <Grid>
+        <Grid.Col span={{ base: 12, md: 10, lg: 10 }}>
+          <Link to={{ pathname: '/' }}>
+            <Flex gap={3} align='center' mb={12}>
+              <Image src={PianoLogoWhite} h={38} />
+              <Image src={MublinLogoWhite} h={28} />
+            </Flex>
+          </Link>
+        </Grid.Col>
+        <Grid.Col span={{ base: 12, md: 2, lg: 2 }}>
+          <ActionIcon 
+            aria-label="Instagram"
+            size='lg'
+            variant='light'
+            color='orange'
+            radius='sm'
+            component='a'
+            target='blank'
+            href='https://instagram.com/mublin'
+            title='instagram.com/mublin'
+          >
+            <IconBrandInstagram style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
+          </ActionIcon>
+        </Grid.Col>
+      </Grid>
+      <Text mt={10} c='dimmed' size='xs'>
+        Business inquiries: fabio@mublin.com
+      </Text>
+      <Text mt={10} ml={4} c='dimmed' size='xs' opacity={0.4}>
         © 2025 Mublin
       </Text>
-      <ActionIcon 
-        size='md'
-        variant='subtle'
-        color='gray'
-        radius='xl'
-        component='a'
-        target='blank'
-        href='https://instagram.com/mublin'
-        title='instagram.com/mublin'
-      >
-        <IconBrandInstagram style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
-      </ActionIcon>
-    </Flex>
+    </Box>
   )
 }
 
